40233e46ec412ce01dfb67c8d2c91ea56478c8c5
Ricardo Cruz <ricardocruz.a89@gmail.com>
2014-09-01 19:30:01 +0000

    Added auth token generation on user creation

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ActiveRecord::Base
+  before_save :ensure_auth_token
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  private
+
+    def ensure_auth_token
+      self.authentication_token = generate_auth_token if authentication_token.blank?
+    end
+
+    def generate_auth_token
+      loop do
+        token = Devise.friendly_token
+        break token unless User.where(authentication_token: token).first
+      end
+    end
 end
