8619d9e1f9ac8f89ab2e2e1a8ec131519f27ff1c
Ricardo Cruz <ricardocruz.a89@gmail.com>
2014-09-02 03:20:36 +0000

    Added an api/users#index route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'sessions' }
+
+  namespace :api, defaults: { format: 'json' } do
+    resources :users, only: [:index]
+  end
 end
