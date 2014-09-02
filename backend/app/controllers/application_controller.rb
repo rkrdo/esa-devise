102fd370a4e884f08e9056650ccc9b92a36a65da
Ricardo Cruz <ricardocruz.a89@gmail.com>
2014-09-02 03:20:12 +0000

    Added authentication before_filter to application_controller

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_filter :authenticate_user_from_token!
+  before_filter :authenticate_user!
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -7,6 +8,9 @@ class ApplicationController < ActionController::Base
 
   private
 
+    # http://apidock.com/rails/v4.0.2/ActionController/HttpAuthentication/Token/token_and_options
+    # Should have an authorization header that looks like this:
+    # Authorization: Token token="abc", nonce="def"
     def authenticate_user_from_token!
       authenticate_with_http_token do |token, options|
         user = User.find_by(email: options[:user_email].presence)
